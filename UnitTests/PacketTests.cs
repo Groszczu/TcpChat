112903a68e4fa9e7223bc2b6d2017691fb171d74
Roch Goszczyński <roch.goszczynski@gmail.com>
2019-11-04 00:30:08 +0100

    small changes in Packet test

--- UnitTests/PacketTests.cs
+++ UnitTests/PacketTests.cs
@@ -19,19 +19,22 @@ namespace UnitTests
             var array = formatter.Serialize(packet);
             var result = Encoding.UTF8.GetString(array);
 
-            const string expected = "id-)00000000-0000-0000-0000-000000000000(|operation-)AcceptInvite(|status-)Ok(|length-)8(|message-)Accepted(|";
+            const string expected =
+                "id-)00000000-0000-0000-0000-000000000000(|operation-)AcceptInvite(|status-)Ok(|length-)8(|message-)Accepted(|";
             Assert.Equal(expected, result);
         }
 
         [Fact]
         public async Task Packet_Should_Deserialize_Correctly()
         {
-            var inputString = "status-)Unauthorized(|operation-)AcceptInvite(|id-)10400050-5006-0070-0809-000060000000(|length-)12(|message-)Hello world!(|";
+            const string inputString =
+                "status-)Unauthorized(|operation-)AcceptInvite(|id-)10400050-5006-0070-0809-000060000000(|length-)12(|message-)Hello world!(|";
             var inputArray = Encoding.UTF8.GetBytes(inputString);
             var memStream = new MemoryStream(inputArray);
             var packet = await new PacketFormatter().DeserializeAsync(memStream);
 
-            var expected = new Packet(Operation.AcceptInvite, Status.Unauthorized, new Guid("10400050-5006-0070-0809-000060000000"), "Hello world!");
+            var expected = new Packet(Operation.AcceptInvite, Status.Unauthorized,
+                new Guid("10400050-5006-0070-0809-000060000000"), "Hello world!");
             Assert.Equal(expected.Id, packet.Id);
             Assert.Equal(expected.Operation, packet.Operation);
             Assert.Equal(expected.Status, packet.Status);
