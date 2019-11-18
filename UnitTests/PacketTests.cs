76b6cef833840d00f2e21b96d99caf4f566af35a
Roch Goszczyński <roch.goszczynski@gmail.com>
2019-11-18 19:16:33 +0100

    update packet unit tests to check new packet field

--- UnitTests/PacketTests.cs
+++ UnitTests/PacketTests.cs
@@ -7,12 +7,12 @@ using Xunit;
 
 namespace UnitTests
 {
-   /* public class PacketTests
+    public class PacketTests
     {
         [Fact]
         public void Packet_Should_Serialize_Correctly()
         {
-            var packet = new Packet(Operation.Invite, Status.Ok, Guid.Empty, new Timestamp(new DateTime(2018, 1, 1)));
+            var packet = new Packet(Operation.Invite, Status.Ok, Guid.Empty,  12345, new Timestamp(new DateTime(2018, 1, 1)));
             packet.SetMessage("Accepted");
             packet.SetDestinationId(12);
             var formatter = new PacketFormatter();
@@ -20,7 +20,7 @@ namespace UnitTests
             var result = Encoding.UTF8.GetString(array);
 
             const string expected =
-                "id-)00000000-0000-0000-0000-000000000000(|operation-)Invite(|status-)Ok(|timestamp-)1514764800(|destination-)12(|length-)8(|message-)Accepted(|";
+                "Identyfikator-)12345(|Operacja-)Invite(|Status-)Ok(|timestamp-)1514764800(|sid-)00000000-0000-0000-0000-000000000000(|destination-)12(|message-)Accepted(|";
             Assert.Equal(expected, result);
         }
 
@@ -28,18 +28,22 @@ namespace UnitTests
         public async Task Packet_Should_Deserialize_Correctly()
         {
             const string inputString =
-                "id-)10400050-5006-0070-0809-000060000000(|operation-)Invite(|status-)Ok(|timestamp-)1514764800(|destination-)12(|length-)8(|message-)Accepted(|";
+                "sid-)10400050-5006-0070-0809-000060000000(|Operacja-)Invite(|Identyfikator-)13013(|Status-)Ok(|timestamp-)1514764800(|destination-)12(|message-)Accepted(|";
             var inputArray = Encoding.UTF8.GetBytes(inputString);
             var memStream = new MemoryStream(inputArray);
             var packet = await new PacketFormatter().DeserializeAsync(memStream);
 
             var expected = new Packet(Operation.Invite, Status.Ok,
-                new Guid("10400050-5006-0070-0809-000060000000"), new Timestamp(new DateTime(2018, 1, 1))).SetDestinationId(12).SetMessage("Accepted");
-                
-            Assert.Equal(expected.Id.ObjectValue, packet.Id.ObjectValue);
-            Assert.Equal(expected.Operation.ObjectValue, packet.Operation.ObjectValue);
-            Assert.Equal(expected.Status.ObjectValue, packet.Status.ObjectValue);
-            Assert.Equal(expected.Message.ObjectValue, packet.Message.ObjectValue);
+                    new Guid("10400050-5006-0070-0809-000060000000"), 13013, new Timestamp(new DateTime(2018, 1, 1)))
+                .SetDestinationId(12).SetMessage("Accepted");
+
+            Assert.Equal(expected.ClientId.Value, packet.ClientId.Value);
+            Assert.Equal(expected.Operation.Value, packet.Operation.Value);
+            Assert.Equal(expected.Status.Value, packet.Status.Value);
+            Assert.Equal(expected.Timestamp.Value, packet.Timestamp.Value);
+            Assert.Equal(expected.SessionId.Value, packet.SessionId.Value);
+            Assert.Equal(expected.DestinationId.Value, packet.DestinationId.Value);
+            Assert.Equal(expected.Message.Value, packet.Message.Value);
         }
-    }*/
+    }
 }
